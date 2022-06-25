b0c971d0bcf3d7327bfda6e9e5da0654fc3f2307
Sandro_330 <sandro-poppe@gmx.de>
2022-06-25 13:24:19 +0200

    dw

--- SandroPoppe/Block3/LightToLoop_AR/05_serial_write_read_multipleValues/05_serial_write_read_multipleValues.ino
+++ SandroPoppe/Block3/LightToLoop_AR/05_serial_write_read_multipleValues/05_serial_write_read_multipleValues.ino
@@ -14,58 +14,58 @@
 Funken fnk;
 rgb_lcd lcd;
 
-// These constants won't change. They're used to give names to the pins used:
-const int ledPin = 5; // digital pin that LED is attahced to
-const int servoPin = 6; // digital pin that servor motor is attahced to
-const int analogInPin = 0; // or A0  // digital input pin that the sensor is attached to
-const int digitalInPin = 2; // or A0  // digital input pin that the sensor is attached to
+
+const int ledPin = 5; 
+const int servoPin = 6; 
+const int analogInPin = 0; 
+const int digitalInPin = 2; 
 const int touchPin = 3;
 const int analogPin = 1;
 String instrumentNameValue;
-int servoValue = 0;           // value used to drive servo motor
-int ledValue = 0;           // pwm value used to drive actuator
-String r1 = "r1";
-String r2 = "r2";
-int potiValue = 0;        // value from potentiometer
+int servoValue = 0;          
+int ledValue = 0;           
+String r1 = "Start by holding";
+String r2 = "the Touch Sensor";
+int potiValue = 0;       
 int switchValue = 0;
-int drehding = 0;// value from switch
+int drehding = 0;
 int touchValue = 0;
 
 unsigned long lastSent = 0;
-int updateSerial = 10; // interval to send value via serial port
+int updateSerial = 10; 
 
-Servo servoMotor;  // create servo object to control a servo
+Servo servoMotor; 
 
 void setup() {
 
-  // init funken
+ 
   lcd.begin(16, 2);
-  fnk.begin(57600, 0, 0); // higher baudrate for better performance
-  fnk.listenTo("RowOne", rowOne); // however you want to name your callback
-  fnk.listenTo("RowTwo", rowTwo); // however you want to name your callback
+  fnk.begin(57600, 0, 0); 
+  fnk.listenTo("RowOne", rowOne); 
+  fnk.listenTo("RowTwo", rowTwo); 
 
-  // define input pins
+
   pinMode(digitalInPin, INPUT);
   lcd.begin(16, 2);
-  // set RGB backlight
+ 
   lcd.setRGB(255, 255, 255);
-  // for analog input + output pin no initialization as an input needed !
+  
 
-  // attaches the servo on servo pin to the servo object
+ 
   servoMotor.attach(servoPin);
 }
 
 void loop() {
 
-  // needed to make FUNKEN work
+
   fnk.hark();
 
-  // send values sensors to P5.js
 
-  // read the analog pin
+
+ 
   potiValue = analogRead(analogInPin);
 
-  // read the digital pin
+
   switchValue = digitalRead(digitalInPin);
 
   drehding = analogRead(analogPin);
@@ -73,27 +73,26 @@ void loop() {
   touchValue = digitalRead(touchPin);
 
 
-  // Do not try to send Serial stuff too often, be prevent this by checking when we sent the last time
+  
   if ((millis() - lastSent) > updateSerial) {
 
-    // message looks like this: "value1 value2 ..."
-    // finish message with a line feed > last message with "Serial.println()" instead of "Serial.print()"
+ 
 
-    // print sensor value
+ 
     Serial.print(potiValue);
-    // SPACE
+
     Serial.print(" ");
-    // print switch value
+  
     Serial.print(switchValue);
     Serial.print(" ");
     
-    // print switch value
+  
     Serial.print(drehding);
     Serial.print(" ");
     Serial.print(touchValue);
     Serial.print(" ");
     
-    // print switch value
+
     Serial.println(r1);
     lcd.clear();
     lcd.setCursor(0, 0);
@@ -101,7 +100,7 @@ void loop() {
     lcd.setCursor(0, 1);
     lcd.print(r2);
     
-    // update timestamp last sent
+
     lastSent = millis();
   }
 
@@ -109,7 +108,7 @@ void loop() {
 
 void rowOne(char *c) {
 
-  // get first argument
+
   char *token = fnk.getToken(c); // is needed for library to work properly, but can be ignored
 
    r1 = fnk.getRemaining();
@@ -119,7 +118,7 @@ void rowOne(char *c) {
 }
 void rowTwo(char *c) {
 
-  // get first argument
+  
   char *token = fnk.getToken(c); // is needed for library to work properly, but can be ignored
    r2 = fnk.getRemaining();
   
--- SandroPoppe/Block3/LightToLoop_P5/sketch.js
+++ SandroPoppe/Block3/LightToLoop_P5/sketch.js
@@ -4,11 +4,10 @@
 // serial magic happens here > "libraries/webserial.js" 
 // good documentation web serial API: https://web.dev/serial/
 
-
 let connectButton;
 let serialController;
 let receivedValues = [];
-let HighF,LowF,G,A,B,C,D,E,Kick,Snare,Metro,MetroUp;
+let HighF,LowF,G,A,B,C,D,E,Kick,Snare,Metro,MetroUp,Clap,Guitar,Metronom;
 let looptime;
 let time = 0;
 var loopedObjects = [];
@@ -24,45 +23,50 @@ let looper = false;
 let playMetro = false;
 let metroDelay = 20;
 let metroBeat = 0;
+let currentMap;
+let volumeString = ">";
+let metroSwitch = 1;
 
 class LoopedSound{
-  constructor(sound, time){
+  constructor(sound, time, name){
     this.sound = sound;
     this.time = time;
-
-  }
-}
-class Sounds{
-  constructor(sound, name){
-    this.sound = sound;
     this.name = name;
   }
 }
+class Sounds{
+  constructor(sound, name, type){
+    this.sound = sound;
+    this.name = name;
+    this.type = type;
+  }
+}
 function preload(){
-    Kick = new Sounds(new Array(loadSound('Data/drumhit_Kick7.wav')),"Kick");
-    Snare = new Sounds(new Array(loadSound('Data/drumhit_snare3.wav')),"Snare");
-    Hihat = new Sounds(new Array(loadSound('Data/drumhit_Hat11.wav')),"Hihat");
-    Piano = new Sounds(new Array(loadSound('Data/piano-a_A_major.wav'),loadSound('Data/piano-b_B_major.wav'),loadSound('Data/piano-c_C_major.wav'),loadSound('Data/piano-d_D_major.wav'),loadSound('Data/piano-e_E_major.wav')),"Piano");
+    Kick = new Sounds(new Array(loadSound('Data/Kick1.wav'),loadSound('Data/Kick2.wav'),loadSound('Data/Kick3.wav'),loadSound('Data/Kick4.wav'),loadSound('Data/Kick5.wav')),"Kick",1);
+    Clap = new Sounds(new Array(loadSound('Data/Clap1.wav'),loadSound('Data/Clap2.wav'),loadSound('Data/Clap3.wav'),loadSound('Data/Clap4.wav'),loadSound('Data/Clap5.wav')),"Clap",1);
+    Snare = new Sounds(new Array(loadSound('Data/Snare1.wav'),loadSound('Data/Snare2.wav'),loadSound('Data/Snare3.wav'),loadSound('Data/Snare4.wav'),loadSound('Data/Snare5.wav')),"Snare",1);
+    Hihat = new Sounds(new Array(loadSound('Data/Hat1.wav'),loadSound('Data/Hat2.wav'),loadSound('Data/Hat3.wav'),loadSound('Data/Hat4.wav'),loadSound('Data/Hat5.wav')),"Hihat",1);
+    Guitar = new Sounds(new Array(loadSound('Data/Guitar1.wav'),loadSound('Data/Guitar2.wav'),loadSound('Data/Guitar3.wav'),loadSound('Data/Guitar4.wav'),loadSound('Data/Guitar5.wav')),"Guitar",1);
+    Piano = new Sounds(new Array(loadSound('Data/piano-a_A_major.wav'),loadSound('Data/piano-b_B_major.wav'),loadSound('Data/piano-c_C_major.wav'),loadSound('Data/piano-d_D_major.wav'),loadSound('Data/piano-e_E_major.wav')),"Piano",2);
     Metro = new Sounds(new Array(loadSound('Data/Metronome.wav')),"Metro");
     MetroUp = new Sounds(new Array(loadSound('Data/MetronomeUp.wav')),"MetroUP");
-   
+    Metronom = new Sounds(null,"Metronom",3);
 }
-
 function setup() {
   looptime = 60 * 4
   cuttentSound = Kick;
   library.push(Kick);
   library.push(Snare);
   library.push(Hihat);
+  library.push(Clap);
+  library.push(Guitar);
   library.push(Piano);
+  library.push(Metronom);
   currentSound = Kick.sound[0];
-  // canvas
-  canvas = createCanvas(640, 480).parent('canvas');
-
-  // init serial connection with baudrate
+  //canvas = createCanvas(640, 480).parent('canvas');
   serialController = new SerialController(57600);
 
-  // init gui
+  
   connectButton = createButton("Initialize Serial Connection");
   connectButton.class("button");
   connectButton.mousePressed(initSerial);
@@ -73,7 +77,14 @@ function draw() {
   if(!started){
     for(let i = 0; i < loopedObjects.length;i++){
     if(loopedObjects[i].time == time){
-      loopedObjects[i].sound.play();
+      if(loopedObjects[i].name =="M"){
+        if(metroSwitch == 1){
+          loopedObjects[i].sound.play();
+        }
+      }else{
+        loopedObjects[i].sound.play();
+      }
+      
     }
   }
   }
@@ -83,36 +94,44 @@ function draw() {
   if(time >= looptime) time = 0;
   for(let i = 0; i < loopedObjects.length;i++){
     if(loopedObjects[i].time == time){
-      loopedObjects[i].sound.play();
+      if(loopedObjects[i].name =="M"){
+        if(metroSwitch == 1){
+          loopedObjects[i].sound.play();
+        }
+      }else{
+        loopedObjects[i].sound.play();
+      }
     }
   }
   
-
+  volumeString =">";
+  for(let k = 0; k < mapped;k++){
+    volumeString += ">";
+  }
  
- 
 
-  // write value to serial port
   serialController.write("RowOne");
   serialController.write(" "); 
-  //print(library[instrument].name);
-  serialController.write("Sound : " + str(library[instrument].name)); // send integer as string
-  serialController.write("\r\n"); // to finish you
+  serialController.write("Sound : " + str(library[instrument].name));
+  serialController.write("\r\n");
   serialController.write("RowTwo");
-  serialController.write(" "); // If sending multiple variables
-  serialController.write("Hoehe : " + str(mapped)); // send integer as string
-  serialController.write("\r\n"); // to finish your message, send a "new line character"
-
-  // instructions
+  serialController.write(" ");
+  if(library[instrument].type == 2){
+    serialController.write("Hoehe : " + str(volumeString));
+  }else if(library[instrument].type == 1){
+    serialController.write("Typ : " + str(volumeString)); 
+  }else{
+    if(metroSwitch == 0){
+      serialController.write("Status : Off"); 
+    }else{
+      serialController.write("Status : On"); 
+    }
+  }
+  serialController.write("\r\n"); 
   fill(255);
-  
-  // just if ready
   if (serialController.read() && serialController.hasData()) {
-    // split string into array
     receivedValues = split(serialController.read(), " ");
-    // show values
     fill(255);
-    print(receivedValues[3]);
-    text("potentiometer: " + receivedValues[3] + "    switch: " + receivedValues[1], 32, height / 2 + 32);
     if(receivedValues[0]>500 && soundPlayed == false){
       soundPlayed = true;
       currentSound.play();
@@ -126,23 +145,37 @@ function draw() {
       if(instrument<library.length-1){
         instrument ++;
         pressed = true;
-        currentSound = library[instrument].sound[0];
+        if(library[instrument].type != 3){
+          currentSound = library[instrument].sound[0];
+        }
+        
       }else{
         instrument = 0;
         
         pressed = true;
-        currentSound = library[instrument].sound[0];
+        if(library[instrument].type != 3){
+          currentSound = library[instrument].sound[0];
+        }
       }
      }
+     if(library[instrument].type != 3){
      if(library[instrument].sound.length > 1){
-        mapped = int(map(receivedValues[2],0,1000,0,library[instrument].sound.length));
+         mapped = int(map(1023-receivedValues[2],0,1023,0,library[instrument].sound.length));
        if(mapped < library[instrument].sound.length){
+        if(currentMap != mapped){
+          library[instrument].sound[mapped].play();
+        }
+        currentMap = mapped;
            currentSound = library[instrument].sound[mapped];
        }else{
          mapped --;
        }
+      }
+     }else{
+        if(receivedValues[2]< 510) metroSwitch = 1;
+        else metroSwitch = 0;
        
-     }
+      }
      if(receivedValues[1] == 0){
        pressed = false;
      }
@@ -157,7 +190,7 @@ function draw() {
   }
   }else{
     if (serialController.read() && serialController.hasData()) {
-      // split string into array
+
       receivedValues = split(serialController.read(), " ");
       if(receivedValues[3] == 1){
         tempTime++;
@@ -167,9 +200,9 @@ function draw() {
         metroDelay--;
         if(metroDelay <= 0){
           if(metroBeat %4 == 0){
-            loopedObjects.push(new LoopedSound(MetroUp.sound[0],time));
+            loopedObjects.push(new LoopedSound(MetroUp.sound[0],time,"M"));
           }else{
-             loopedObjects.push(new LoopedSound(Metro.sound[0],time));
+             loopedObjects.push(new LoopedSound(Metro.sound[0],time,"M"));
           }
           metroBeat++;
           metroDelay = 40;
@@ -181,13 +214,13 @@ function draw() {
       }
     }
   }
-  // background
+
   
  
 
 }
 
-// init serial connection
+
 function initSerial() {
   serialController.init();
 }
